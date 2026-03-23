33ddf07a3fdae0f80eda307eaed324ac59561cc9
takoumenecarel-gh <takoumenecarel@icloud.com>
2026-03-23 17:53:02 -0600

    adding java dependencies files

---
+++ src/test/java/com/portfolio/my_portfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.my_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
