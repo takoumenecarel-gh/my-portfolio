e165c72c3903b6dab7ed6b032caa4548b73a90a0
takoumenecarel-gh <takoumenecarel@icloud.com>
2026-03-24 00:44:38 -0600

    adding new feature

---
+++ src/main/java/com/portfolio/my_portfolio/HomeController.java
@@ -0,0 +1,13 @@
+package com.portfolio.my_portfolio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 Portfolio is running on EKS!";
+    }
+}
